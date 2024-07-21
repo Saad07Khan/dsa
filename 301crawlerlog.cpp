fef49f95ee2bc2cf8603cc24df6a93d7b33d05a4
Saad Khan <saadmohammed.khan07@gmail.com>
2024-07-21 18:20:01 +0530

    302

--- 301crawlerlog.cpp
+++ 301crawlerlog.cpp
@@ -27,3 +27,51 @@ public:
         return count;
     }
 };
+
+
+sort colors brute force
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // Initialize counters for 0s, 1s, and 2s
+        int count0 = 0, count1 = 0, count2 = 0;
+        
+        // Count the number of 0s, 1s, and 2s
+        for(int num : nums) {
+            if(num == 0) {
+                count0++;
+            } else if(num == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+        
+        // Overwrite the array with the sorted colors
+        int index = 0;
+        for(int i = 0; i < count0; i++) {
+            nums[index] = 0;
+            index++;
+        }
+        for(int i = 0; i < count1; i++) {
+            nums[index] = 1;
+            index++;
+        }
+        for(int i = 0; i < count2; i++) {
+            nums[index] = 2;
+            index++;
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
