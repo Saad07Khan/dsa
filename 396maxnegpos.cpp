efa56ed10158dc2b7bc5c401046cb10fda253f83
Saad Khan <saadmohammed.khan07@gmail.com>
2025-03-20 14:50:06 +0530

    new

---
+++ 396maxnegpos.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int neg=0;
+        int pos=0;
+        int maxi=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<0)
+            {
+                neg++;
+            }
+            if(nums[i]>0)
+            {
+                pos++;
+            }
+            maxi=max(maxi,(max(pos,neg)));
+        }
+        return maxi;
+        
+    }
+};
